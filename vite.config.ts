49b6914d55ba0dd643cb31f260f86bf1cd1cffe1
kinglsayer4581 <148754404+kingslayer458@users.noreply.github.com>
2025-03-07 13:47:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/reacter-steam/", // 👈 Add this line
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
